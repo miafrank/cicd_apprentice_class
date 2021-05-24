68921df829fcb93b82a92034c75abcaa6e62c381
Mia Frank <mia.frank@madeintandem.com>
2021-05-24 16:02:01 -0500

    added basic example for ci cd class

---
+++ cicd_class/phonebook.py
@@ -0,0 +1,2 @@
+def phone_numbers(numbers):
+  return {f'Person: {k} Phone Number: {v}' for k, v in numbers.items()}
---
+++ tests/test_unit_testing_example.py
@@ -0,0 +1,6 @@
+from cicd_class.phonebook import *
+
+def test_phone_numbers(): 
+  test_phone_book = {"Janae": "314986448"}
+  result = phone_numbers(test_phone_book)
+  assert result == {'Person: Janae Phone Number: 314986448'}

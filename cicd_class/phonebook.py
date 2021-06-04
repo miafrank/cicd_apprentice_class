4a6e6b89d88e190e2b51a990e2460026d4569a40
Mia Frank <mia.frank@madeintandem.com>
2021-06-04 13:28:47 -0500

    i am the best dev of all time

--- cicd_class/phonebook.py
+++ cicd_class/phonebook.py
@@ -1,5 +1,5 @@
 def phone_numbers(numbers):
-    result = ""
-    for k, v in numbers.items():
-        result += f'Person: {k} Phone Number: {v}\n'     
-    return result
+  result = ""
+  for k, v in numbers.items():
+    result += f'Person: {k} Phone Number: {v}\n'     
+  return result

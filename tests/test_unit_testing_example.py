827fdc3d9680bdcc5b631b8cffbfc7225eaa90c1
Mia Frank <mia.frank@madeintandem.com>
2021-06-04 13:27:22 -0500

    updated test

--- tests/test_unit_testing_example.py
+++ tests/test_unit_testing_example.py
@@ -2,6 +2,6 @@ from cicd_class.phonebook import phone_numbers
 
 
 def test_phone_numbers():
-    test_phone_book = {"Janae": "314986448"}
+    test_phone_book = {"Janae": "3149846448", "Mia": "6364484018"}
     result = phone_numbers(test_phone_book)
-    assert result == {'Person: Janae Phone Number: 314986448'}
+    assert result == "Person: Janae Phone Number: 3149846448\nPerson: Mia Phone Number: 6364484018\n"
